27c3dad1f264151652d0087c5ef12bdfe4c6655f
Nick Arcuri <nickarcuri@protonmail.com>
2024-12-23 12:31:13 -0500

    Temporarily remove navigation

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,12 @@
-const NavLink = ({ children }: { children: React.ReactNode }) => {
-    return <li className="tracking-tighter text-lg">{children}</li>;
-};
+// const NavLink = ({ children }: { children: React.ReactNode }) => {
+//     return <li className="tracking-tighter text-lg">{children}</li>;
+// };
+//
+// const NavList = ({ children }: { children: React.ReactNode }) => {
+//     return (
+//         <ul className="flex flex-col gap-2 content-center pl-6">{children}</ul>
+//     );
+// };
 
 export const Header = () => {
     // TODO: Film leader font???
@@ -9,14 +15,6 @@ export const Header = () => {
             <div className="sticky top-0">
                 <div className="p-8 max-w-72 flex flex-col gap-10">
                     <h1 className="font-serif text-5xl tracking-tightest">Nick Arcuri</h1>
-                    <ul className="flex flex-col gap-2 content-center pl-6">
-                        <NavLink>Home</NavLink>
-                        <NavLink>
-                            <a href="/about">About</a>
-                        </NavLink>
-                        <NavLink>Projects</NavLink>
-                        <NavLink>Contact</NavLink>
-                    </ul>
                 </div>
             </div>
         </header>
